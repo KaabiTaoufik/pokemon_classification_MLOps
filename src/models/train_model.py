d09657325fc03db7ba2333a1b372e0b575740f36
KaabiTaoufik <taoufik_c10@hotmail.com>
2023-06-02 19:04:48 +0100

    fit the training model

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,13 +1,6 @@
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, GlobalAveragePooling2D
-from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras.models import Sequential
-from keras import applications
-from keras.utils import plot_model
-
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=7)
-filepath = "model.h5"
-ckpt = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
-rlp = ReduceLROnPlateau(monitor='loss', patience=3, verbose=1)
+import mlflow
 
 
 # defining model
@@ -21,8 +14,10 @@ def cnn(image_size, num_classes):
     classifier.add(Dense(num_classes, activation='softmax'))
     classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
     return classifier
-def train_model(image_size, num_classes):
-    neuralnetwork_cnn = cnn(image_size, num_classes)
-    neuralnetwork_cnn.summary()
-    plot_model(neuralnetwork_cnn, show_shapes=True)
-    return neuralnetwork_cnn
+def train_model(model, training_set, validation_data, epochs = 2):
+    with mlflow.start_run():
+        mlflow.tensorflow.log_model(model, "model")
+        mlflow.tensorflow.autolog()
+        history = model.fit(training_set, validation_data = validation_data, epochs = epochs)
+        mlflow.tensorflow.log_model(model, "model")
+        return model, history
