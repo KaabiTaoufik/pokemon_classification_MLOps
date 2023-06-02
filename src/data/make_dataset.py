c44d977f83804a0e7dc04c3c84df91d18fd3f4fd
azer <azerchabbar@gmail.com>
2023-06-02 20:55:41 +0100

    Splitting data into test and training

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,3 +1,5 @@
 import os
+os.chdir('data')
+os.system('kaggle datasets download -d bhawks/pokemon-generation-one-22k && unzip \*.zip && rm \*.zip')
 
-os.system('kaggle datasets download -d bhawks/pokemon-generation-one-22k')
+
