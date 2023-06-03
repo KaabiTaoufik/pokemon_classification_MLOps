f6c9f4eb5159bfb37b60fb9d2121644abf47690b
KaabiTaoufik <taoufik_c10@hotmail.com>
2023-06-03 09:17:15 +0100

    faza

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,11 +1,10 @@
 from src.constants import *
-from src.models.train_model import cnn, train_model
+from src.models.train_model import train_model
 from src.mlflow.mlflow_experiment import initiate_mlflow_experiment
 from src.features.build_features import build_features
 from src.models.evaluate_model import evaluate_model
 
 from opentelemetry import trace
-from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter
