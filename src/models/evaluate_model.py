09ddbaa056180eb72cb373013b57ba9a0ec91cd2
azer <azerchabbar@gmail.com>
2023-06-03 08:42:27 +0100

    Added dvc

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -3,17 +3,10 @@ from tensorflow.keras.metrics import Accuracy, Recall, AUC
 import mlflow
 
 def evaluate_model(model: tf.keras.Model, validation_dataset: tf.data.Dataset):
-        accuracy_metric = Accuracy()
-        recall_metric = Recall()
-        auc_metric = AUC()
-        for images, labels in validation_dataset:
-            predictions = model.predict(images)
-            accuracy_metric.update_state(labels, predictions)
-            recall_metric.update_state(labels, predictions)
-            auc_metric.update_state(labels, predictions)
-        accuracy = accuracy_metric.result().numpy()
-        recall = recall_metric.result().numpy()
-        auc = auc_metric.result().numpy()
-        mlflow.log_metric("accuracy", accuracy)
-        mlflow.log_metric("recall", recall)
-        mlflow.log_metric("auc", auc)
+        with mlflow.start_run():
+            loss, accuracy ,  auc , recall  , precision = model.evaluate(validation_dataset)
+            mlflow.log_metric("precision", precision)
+            mlflow.log_metric("loss", loss)
+            mlflow.log_metric("accuracy", accuracy)
+            mlflow.log_metric("recall", recall)
+            mlflow.log_metric("auc", auc)
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,6 +1,7 @@
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras.models import Sequential
 from keras.callbacks import ModelCheckpoint
+import tensorflow as tf
 import mlflow.keras
 
 
@@ -13,7 +14,7 @@ def cnn(image_size, num_classes):
     classifier.add(MaxPooling2D(pool_size=(2, 2)))
     classifier.add(Flatten())
     classifier.add(Dense(num_classes, activation='softmax'))
-    classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+    classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc',tf.keras.metrics.AUC(from_logits=True),tf.keras.metrics.Recall(),tf.keras.metrics.Precision()])
     return classifier
 def train_model(model, training_dataset, validation_dataset, epochs = 3):
     with mlflow.start_run():
